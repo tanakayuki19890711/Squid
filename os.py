fbea4d77dcb1057a4cea00615ad7844e2d08f32e
tanakayuki <yuki.11.soccer.fukui@gmail.com>
2023-03-11 20:39:44 +0900

    first-commit

--- os.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ['HTTP_PROXY'] == 'http://cached-proxy:3128'
-os.environ['http_proxy'] == 'http://cached-proxy:3128'
-os.environ['HTTPS_PROXY'] == 'http://cached-proxy:3128'
-os.environ['https_proxy'] == 'http://cached-proxy:3128'
-
-print(os.environ['https_proxy'])
